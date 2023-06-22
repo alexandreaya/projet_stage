6252319426f04cc0e369c073b6540087a11172cf
alexandre <126798912+alexandreaya@users.noreply.github.com>
2023-06-22 16:02:28 +0200

    Update extract_block.py

--- extract_block.py
+++ extract_block.py
@@ -1,7 +1,7 @@
 # Code pour obtenir le dernier bloc etherum et le solde d'un compte à partir de son adresse
 
 from web3 import Web3
-infura_url = "https://mainnet.infura.io/v3/91e6692c109c47c395f5d115a5a01395"
+infura_url = "https://mainnet.infura.io/v3/clé_infura"
 web3 = Web3(Web3.HTTPProvider(infura_url))
 
 print (web3.is_connected())
